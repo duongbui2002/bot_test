575408e13dc9e714d4dd94582ecab56d87cab7ab
duongbui2002 <duongbuidinh593@gmail.com>
2022-11-21 09:29:27 +0700

    feat unsub: implement unsub with id

---
+++ src/commands/unsub.ts
@@ -0,0 +1,22 @@
+import TelegramBot, {Message} from "node-telegram-bot-api";
+import {SubModel} from "@/models/sub.model";
+
+export default async function (bot: TelegramBot, msg: Message, command, commandName) {
+  if (!commandName) {
+    await bot.sendMessage(msg.chat.id, 'Project Id must be required.')
+    return
+  }
+
+  const result = await SubModel.deleteOne({projectId: commandName, userID: msg.from.id});
+
+  if (result.deletedCount === 0) {
+    await bot.sendMessage(msg.chat.id, `Project with #${commandName} does not exist`)
+    return
+  }
+
+
+  await bot.sendMessage(msg.chat.id, ` The subscription project with #${commandName} was removed`)
+
+  return
+
+}
