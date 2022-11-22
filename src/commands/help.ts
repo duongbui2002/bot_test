2fd118f76dd876a75fc2ed9b5645da4b0185f449
duongbui2002 <duongbuidinh593@gmail.com>
2022-11-22 15:04:06 +0700

    change command: mysubs to chatsubs

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,7 +3,7 @@ import TelegramBot, {Message} from "node-telegram-bot-api";
 export default async function (bot: TelegramBot, msg: Message, command: string, commandName: string, user: any) {
   const response =
     `<b>You can control me by sending these commands:</b> 
-  +) /mysubs - See all your subscription projects
+  +) /chatsubs - See all your subscription projects
   +) /projects - See all available projects that you can subscribe to notifications
   +) /sub {id} - Subscribe to notifications about project with {id}
   +) /unsub {id} - Unsubscribe to notifications about project with {id} 
