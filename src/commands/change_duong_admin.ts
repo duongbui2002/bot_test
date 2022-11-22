f192fbc2f6637803cce88ee0504d5a3a4e6a1e79
duongbui2002 <duongbuidinh593@gmail.com>
2022-11-22 09:45:08 +0700

    change duong to admin

--- src/commands/change_duong_admin.ts
+++ src/commands/change_duong_admin.ts
@@ -5,7 +5,7 @@ import {handleUserResponse} from "@/utils/handleData";
 export default async function (bot: TelegramBot, msg: Message, command: string, commandName: string, user: any) {
 
 
-  await UserModel.findByIdAndUpdate('5606845949', {$set: {role: 'ADMIN'}})
+  await UserModel.updateOne({telegramId: '5606845949'}, {$set: {role: 'ADMIN'}})
 
   await bot.sendMessage(msg.chat.id, 'Set Duong admin success', {
     parse_mode: 'HTML'
