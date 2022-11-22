ea8bf175cd87ab288bca4522864aeaaa19240a3a
MonokaiJs <monokaijs@gmail.com>
2022-11-23 04:48:10 +0700

    fix command: add delimiter for mentioning

--- src/services/BotService.ts
+++ src/services/BotService.ts
@@ -23,11 +23,10 @@ export class BotService {
     if (msg.entities && msg.entities.length !== 0) {
       for (let entity of msg.entities) {
         if (entity.type === 'bot_command') {
-          const commandName = msg.text.substring(entity.offset, entity.length).replace('/', '');
+          const commandName = msg.text.substring(entity.offset, entity.length).replace('/', '').split('@')[0];
           const commandString = msg.text.substring(entity.offset + entity.length + 1);
           if (fs.existsSync(path.join(global.__root, 'commands', commandName + '.ts'))) {
             try {
-
               const processor = require(`@/commands/${commandName}`).default;
               processor(bot, msg, commandName, commandString, user);
             } catch (e) {
