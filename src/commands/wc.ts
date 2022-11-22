4d1ba45aa03c2a864e19a815dae0b2a6715b601a
MonokaiJs <monokaijs@gmail.com>
2022-11-23 04:16:29 +0700

    feat wc: world cup news

--- src/commands/wc.ts
+++ src/commands/wc.ts
@@ -3,11 +3,6 @@ import {WorldCupService} from "@/services/WorldCupService";
 import requireRoleMiddleware, {Role} from "@/middlewares/requireRole.middleware";
 
 export default async function (bot: TelegramBot, msg: Message, command: string, commandName: string, user: any) {
-  let isPermitted = requireRoleMiddleware(user, Role.Admin);
-  if (!isPermitted) {
-    await bot.sendMessage(msg.chat.id, `You are not allowed to do this command`)
-    return
-  }
   const matches = await WorldCupService.getCurrentMatches();
   if (matches) {
     for (let match of matches) {
