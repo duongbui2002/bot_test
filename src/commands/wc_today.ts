8b9546b90102573ccc0c5e91c1e74f1b30c8413e
MonokaiJs <monokaijs@gmail.com>
2022-11-23 05:04:15 +0700

    fix wc_today: fix wrong phrases

--- src/commands/wc_today.ts
+++ src/commands/wc_today.ts
@@ -4,10 +4,10 @@ import moment from "moment-timezone";
 
 export default async function (bot: TelegramBot, msg: Message, command: string, commandName: string, user: any) {
   const start = new Date();
-  start.setUTCHours(0,0,0,0);
+  start.setUTCHours(0, 0, 0, 0);
   const end = new Date();
   end.setDate(end.getDate() + 1)
-  end.setUTCHours(23,59,59,999);
+  end.setUTCHours(23, 59, 59, 999);
 
   const matches = await WorldCupService.getMatches();
   const todayMatches = matches.filter(match => {
@@ -18,11 +18,11 @@ export default async function (bot: TelegramBot, msg: Message, command: string, 
   }
   moment.locale('vi');
   for (let match of todayMatches) {
-    const isFinished = match.status === 'Match Finished';
-    const isStarted = !isFinished && match.status !== 'Not Started';
-    await bot.sendMessage(msg.chat.id, `${moment(match.event_timestamp * 1000).tz('Asia/Ho_Chi_Minh').calendar()}\n` +
-    `${match.away_team.team_name} vs ${match.home_team.team_name} (${isFinished ? 'Đã kết thúc' : isStarted ? 'Đang diễn ra': 'Chưa bắt đầu'})` +
-      (isStarted || isFinished) ? `Tỉ số ${match.score.fulltime}` : '');
+    const isFinished = match.status_short === 'FT';
+    const notStarted = !isFinished || (match.status_short !== 'NS');
+    const matchTime = moment(match.event_timestamp * 1000).tz('Asia/Ho_Chi_Minh').calendar();
+    const matchStatus = isFinished ? 'Đã kết thúc' : notStarted ? 'Chưa bắt đầu' : 'Đang diễn ra';
+    const teamsPhrase = `${match.away_team.team_name} - ${match.home_team.team_name}`;
+    await bot.sendMessage(msg.chat.id, `${matchTime} (${matchStatus})\n${teamsPhrase}\n\n${match.score && match.score.fulltime ? `Tỉ số ${match.score.fulltime}`:""}`);
   }
-  console.log(matches);
 }
