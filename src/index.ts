afc0ecc347c90712c7d4acb653c58da79cda62f5
Edward Nguyen <monokaijs@gmail.com>
2022-12-27 10:23:50 +0000

    Enable Garena Watcher

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ dotenv.config();
 import {BotService} from "@/services/BotService";
 import {WebServer} from "./services/WebServer";
 import {DatabaseService} from "@/services/DatabaseService";
-import {WorldCupService} from "@/services/WorldCupService";
+import {GarenaWatcherService} from "@/services/GarenaWatcherService";
 
 global.__root = __dirname;
 DatabaseService.connect().then(() => {
   console.log('Database has been connected.');
   WebServer.register(() => {
     BotService.register();
-    WorldCupService.register();
+    GarenaWatcherService.register();
   });
 });
