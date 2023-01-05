1012ccc250e58f6d70af2fd5f89f446a7ddf3ff1
MonokaiJs <monokaijs@gmail.com>
2023-01-05 20:10:12 +0700

    :zap: fix removing wrong project

--- src/commands/set_default_project.ts
+++ src/commands/set_default_project.ts
@@ -21,7 +21,7 @@ export default async function (bot: TelegramBot, msg: Message, command, commandN
     await bot.sendMessage(msg.chat.id, 'Project does not exist!')
     return
   }
-  const existProject = await SubModel.findOne({projectId: project.id, messageID: msg.chat.id})
+  const existProject = await ChatProjectModel.findOne({projectId: project.id, messageID: msg.chat.id})
   if (existProject) {
     await existProject.deleteOne();
   }
